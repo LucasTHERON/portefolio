30b08a370032a56d0afe8cc89ccd5d6556d0d2cb
Lucas <lucastheron@hotmail.fr>
2022-11-17 17:18:11 +0100

    importation du svelte

---
+++ portefolio/src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Accueil from "./lib/Accueil.svelte";
+  import Competences from "./lib/Competences.svelte";
+  import Projets from "./lib/Projets.svelte";
+  let content = 'accueil';
+</script>
+
+<main>
+<div class="main-container {content}">
+
+
+  <nav>
+    <button on:click={() => content = 'accueil'}>Accueil</button>
+    <button on:click={() => content = 'competences'}>Compétences</button>
+    <button on:click={() => content = 'projets'}>Projets</button>
+  </nav>
+  
+  <div><h1>
+    {#if content == 'accueil'}
+    <Accueil />
+    {:else if content == 'competences'}
+    <Competences />
+    {:else if content == 'projets'}
+    <Projets />
+    {:else}
+    <h1>404</h1>
+    {/if}        
+  </h1></div>
+
+</div>
+
+
+</main>
+
+<style>
+</style>
---
+++ portefolio/src/lib/Projets.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Datas from './projets/test.json';
+
+    let projetsArray = [];
+    projetsArray.push(Datas);
+    console.log(projetsArray[0][0].Name);
+</script>
+
+<h1>Projets</h1>
+<hr>
+<p>{#each projetsArray as data}
+    <p>{data[0][0]}</p>
+{/each}</p>
+<hr>
